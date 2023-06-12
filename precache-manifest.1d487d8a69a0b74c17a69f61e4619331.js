7bfe4b7551c1c03c1476b00525a53f1757b44cea
Christian Schwenk <christian-schwenk@gmx.de>
2023-06-12 11:36:53 +0200

    Updates

---
+++ precache-manifest.1d487d8a69a0b74c17a69f61e4619331.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b08d0a6ba3aaae1cf28ffa09df0b78e",
+    "url": "/ReactTetris/index.html"
+  },
+  {
+    "revision": "5107f2a629ddba0c6a61",
+    "url": "/ReactTetris/static/css/main.225df9ef.chunk.css"
+  },
+  {
+    "revision": "d41d9e19e5f9bd19872f",
+    "url": "/ReactTetris/static/js/2.50b86302.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ReactTetris/static/js/2.50b86302.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5107f2a629ddba0c6a61",
+    "url": "/ReactTetris/static/js/main.7ca17289.chunk.js"
+  },
+  {
+    "revision": "55b3360fa3a5ede413ef",
+    "url": "/ReactTetris/static/js/runtime-main.51bac490.js"
+  }
+]);
